4f9a66e096009964906d041a0a21facb19a40276
Jesse Wolgamott <jesse@comalproductions.com>
2014-07-07 14:33:18 -0500

    How to parse dates

---
+++ dates.rb
@@ -0,0 +1,16 @@
+
+
+#when your field is a "date"
+# "year-month-day"
+
+Date.parse "2014-07-07"
+
+"07/07/2014"
+"07/07/2014"
+
+"12/3/2014"
+"3/12/2014"
+
+# when your field is a "datetime"
+
+DateTime.parse
